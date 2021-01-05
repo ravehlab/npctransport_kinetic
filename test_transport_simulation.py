0fc43c46427025a67b8cfaf47efea04cfd259c9e
Barak Raveh <barak.raveh@gmail.com>
2021-01-05 13:49:33 +0200

    added unit test for a default simulation

--- test_transport_simulation.py
+++ test_transport_simulation.py
@@ -34,5 +34,44 @@ class TestTransport_Simulation(unittest.TestCase):
         print("Cargo after: {}".format(ts.get_total_cargo_nmol()))
         #return c_arr[:,:-1]
 
+    def test_simulation(self):
+        expected_stats= \
+            {'time_sec': [1.000e-03, 2.501e+00, 5.001e+00, 7.501e+00],
+             'complexL_NPC_N_import': [    0.        , 21623.81698188, 21278.51029702, 21176.81632042],
+             'complexL_NPC_C_import': [  625.        , 21681.06379079, 21334.39189677, 21231.62598839],
+             'complexU_NPC_N_import': [0., 0., 0., 0.],
+             'complexU_NPC_C_import': [0., 0., 0., 0.],
+             'complexL_NPC_N_export': [    0.        , 13545.21145814, 17168.79328743, 19146.95996426],
+             'complexL_NPC_C_export': [    0.        , 13513.62995572, 17132.5458864 , 19107.47897038],
+             'complexU_NPC_N_export': [0., 0., 0., 0.],
+             'complexU_NPC_C_export': [0., 0., 0., 0.],
+             'complexL_C': [74668.81875   , 20207.78255697, 25699.87742521, 30248.80231033],
+             'freeL_C': [225806.1805    , 178129.82134661, 141282.393471  , 113145.48187596],
+             'complexU_C': [0., 0., 0., 0.],
+             'freeU_C': [0., 0., 0., 0.],
+             'complexL_N': [   0.        , 4433.24433551, 6522.12774267, 8448.79229082],
+             'freeL_N': [7.50000000e-04, 2.79654296e+04, 5.06813600e+04, 6.85940423e+04],
+             'complexU_N': [0., 0., 0., 0.],
+             'freeU_N': [0., 0., 0., 0.],
+             'import_L': [0.        , 0.90171421, 0.48877104, 0.35666296],
+             'export_L': [0.        , 0.06811956, 0.10259178, 0.13324456],
+             'import_U': [0., 0., 0., 0.],
+             'export_U': [0., 0., 0., 0.],
+             'GTP_N': [78196.06103896, 63317.01960248, 56680.03947056, 53731.94361351],
+             'GTP_C': [50.83506494, 45.2034439 , 43.17588964, 42.61883649],
+             'GDP_N': [ 93.83370909, 156.21436088, 156.21638929, 156.21694678],
+             'GDP_C': [ 78231.27018701,  93053.56259274,  99692.56825052, 102641.22060322]
+             }
+        ts= transport_simulation.TransportSimulation()
+        ts.bleach_start_time_sec= 100.0
+        ts.dt_sec= 1e-3 
+        sim_flags= dict()#rate_free_to_complex_per_sec=1.0,
+           #max_passive_diffusion_rate_nmol_per_sec_per_M=2e7)
+        sim_time_sec= 10.0
+        stats=ts.simulate(sim_time_sec, nskip_statistics= 2500)
+        for key,expected_v in expected_stats.items():
+            for x,y in zip(stats[key], expected_v):
+                self.assertTrue(np.isclose(x, y, atol= 1e-12, rtol=1e-5))
+        
 if __name__ == '__main__':
     unittest.main()
