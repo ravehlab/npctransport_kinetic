b0a0b69162a170c188a27303f8a1d9e86f4cd0eb
Barak Raveh <barak.raveh@gmail.com>
2021-01-13 22:23:56 +0200

    adapt to nuclear import updates + support a generalized transport simujlation generator + minors

--- map_param_grid.py
+++ map_param_grid.py
@@ -37,16 +37,16 @@ def get_tau_passive_diffusion(nmol_per_sec_per_M, volume_L):
 
 
 def get_new_transport_simulation(**kwargs):
-    ts= transport_simulation.TransportSimulation(**kwargs)        
-#    ts.set_params(rate_GDP_C_to_GDP_N_per_sec=7)   
-    ts.set_params(dt_sec=1e-3)
+    ts= transport_simulation.TransportSimulation(**kwargs) 
+    ts.set_time_step(1e-3)
     return ts
 
 def mp_do_simulation(param_range, i, j, 
-                     equilibration_time_sec):
-    init_params= {"Ran_cell_M": 20e-6}
+                     equilibration_time_sec,
+                    transport_simulation_generator):
+
     nskip_statistics= 100
-    my_ts= get_new_transport_simulation(**init_params)
+    my_ts= transport_simulation_generator()
     cur_params= { param_range['tag_x']: param_range['range_x'][i],
                   param_range['tag_y']: param_range['range_y'][j]}
     my_ts.set_params(**cur_params)
@@ -76,7 +76,8 @@ def mp_handle_error(error):
     
 def map_param_grid_parallel(param_range, 
                    equilibration_time_sec= 150.0, 
-                   n_processors= 5
+                   n_processors= 5,
+                   transport_simulation_generator= get_new_transport_simulation
                   ):
     '''
     Run in parallel to compute simulation results over a range of parameters
@@ -94,7 +95,7 @@ def map_param_grid_parallel(param_range,
     VERBOSE= True
     nx= len(param_range['range_x'])
     ny= len(param_range['range_y'])
-    ts= get_new_transport_simulation()
+    ts= transport_simulation_generator() # a sample ts to be returned
     if VERBOSE:
         for param in [param_range['tag_x'], param_range['tag_y']]:
             print("Param {:} default value is {:}".format(param, getattr(ts, param)))
@@ -107,7 +108,8 @@ def map_param_grid_parallel(param_range,
             jobs_params.append((param_range.copy(),
                                 i,
                                 j,
-                                equilibration_time_sec))
+                                equilibration_time_sec,
+                               transport_simulation_generator))
     print("njobs={}".format(len(jobs_params)))
     callback_function = \
         lambda mydict: mp_handle_stats(stats_grids, mydict)
@@ -119,7 +121,7 @@ def map_param_grid_parallel(param_range,
     results.wait()
     pool.close()
     pool.join()
-    return stats_grids
+    return stats_grids, ts
 
 
 
@@ -142,7 +144,8 @@ def plot_param_grid(param_range,
     ax.set_ylabel(param_range['pretty_y'])
     xlim= ax.get_xlim()
     ylim= ax.get_ylim()
-    ax.set_xlim(xlim[1], xlim[0])
+    if xlim[1]>xlim[0]:
+        ax.set_xlim(xlim[1], xlim[0])
     print(ax.get_xticks())
     cb = plt.colorbar(label= Z_label)
     ticks = cb.get_ticks()
@@ -155,11 +158,12 @@ def get_N_to_C_ratios(stats_grids, v_N_L, v_C_L):
     ratios= (nNs/v_N_L) / (nCs/v_C_L)
     return ratios
 
-def plot_NC_ratios(param_range, stats_grids, ts, ax=None):
+def plot_NC_ratios(param_range, stats_grids, ts, vmax= None):
     NC_ratios= get_N_to_C_ratios(stats_grids, 
                               v_N_L= ts.get_v_N_L(), 
                               v_C_L= ts.get_v_C_L())
-    vmax= 4.0
+    if vmax==None:
+        vmax= 4.0
     plot_param_grid(param_range, 
                     NC_ratios,
                     Z_label= "N/C ratio",
@@ -187,15 +191,20 @@ def plot_bound_fraction(param_range, stats_grids, compartment, ax= None):
 
 def get_import_export_ratios(stats_grids):
     ''' return import/export ratios from stats_grids computed in the previous cell'''
-    import_rate= stats_grids["import_L"]+stats_grids["import_U"]
-    export_rate= stats_grids["export_L"]+stats_grids["export_U"]
+    import_rate= stats_grids["nuclear_importL_per_sec"]+stats_grids["nuclear_importU_per_sec"]
+    export_rate= stats_grids["nuclear_exportL_per_sec"]+stats_grids["nuclear_exportU_per_sec"]
     ratios= import_rate/export_rate
     return ratios
     
-def plot_import_export():
-    ##### Contourf
-    for tag in ['import', 'export']:
-        plt.figure()
+def plot_import_export(param_range, stats_grids, axes= None,
+                      **contourf_kwargs):
+    if axes==None:
+        fig, axes= subplot(2, 1)
+    for tag, ax in zip(['import', 'export'], axes):
+        plt.sca(ax)
         plot_param_grid(param_range, 
-                        stats_grids[f'{tag}_L'],
-                        Z_label= tag + r' [$sec^{-1}$]')
+                        stats_grids[f'nuclear_{tag}L_per_sec'],
+                        Z_label= tag + r' [$sec^{-1}$]',
+                       **contourf_kwargs)
+
+# %%
