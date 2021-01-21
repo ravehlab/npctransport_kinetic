5af1d45fdd2967bc53118875a775a6d180b4d02a
Barak Raveh <barak.raveh@gmail.com>
2021-01-21 08:03:49 +0200

    minor formatting and debug logging

--- map_param_grid.py
+++ map_param_grid.py
@@ -51,7 +51,9 @@ def mp_do_simulation(param_range, i, j,
                   param_range['tag_y']: param_range['range_y'][j]}
     my_ts.set_params(**cur_params)
     stats = my_ts.simulate(equilibration_time_sec,
-                           nskip_statistics= 100)   
+                           nskip_statistics= 100)  
+    if (np.random.rand()<0.2):
+        print(f"Finished ~5 jobs (at i={i} j={j})")
     return {"i":i, "j":j, "stats":stats}
 
 def mp_handle_stats(stats_grids, mydicts):
@@ -146,7 +148,6 @@ def plot_param_grid(param_range,
     ylim= ax.get_ylim()
     if xlim[1]>xlim[0]:
         ax.set_xlim(xlim[1], xlim[0])
-    print(ax.get_xticks())
     cb = plt.colorbar(label= Z_label)
     ticks = cb.get_ticks()
     cb.set_ticks(ticks)
@@ -158,18 +159,17 @@ def get_N_to_C_ratios(stats_grids, v_N_L, v_C_L):
     ratios= (nNs/v_N_L) / (nCs/v_C_L)
     return ratios
 
-def plot_NC_ratios(param_range, stats_grids, ts, vmax= None):
+def plot_NC_ratios(param_range, stats_grids, ts, vmin= 1.0, vmax= 4.0):
     NC_ratios= get_N_to_C_ratios(stats_grids, 
                               v_N_L= ts.get_v_N_L(), 
                               v_C_L= ts.get_v_C_L())
-    if vmax==None:
-        vmax= 4.0
+    print(vmin, vmax)
     plot_param_grid(param_range, 
                     NC_ratios,
                     Z_label= "N/C ratio",
-                    vmin=1.0, 
-                    vmax=vmax, 
-                    levels=np.linspace(1.0,vmax,21), 
+                    vmin= vmin,
+                    vmax= vmax, 
+                    levels=np.linspace(vmin, vmax,21), 
                     extend='both')
 
 def plot_bound_fraction(param_range, stats_grids, compartment, ax= None):
