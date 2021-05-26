fc68f23b8e8284111ad74ccc0ab9a27dd2367f3e
Barak Raveh <ravehb@cs.huji.ac.il>
2021-05-26 10:54:22 +0300

    fix range

--- slurm/collect_results.sh
+++ slurm/collect_results.sh
@@ -3,7 +3,8 @@
 mkdir grid
 ###for x_start in 20.0 30.0 40.0; do 
 for x_start in 10.0 20.0 30.0 40.0 100.0; do 
-    for scale in 1.5 2.5 5.0 10.0; do 
+
+    for scale in 1.5 2.5 5.0 10.0 15.0 20.0; do 
         x_end=`echo $x_start $scale | awk '{printf "%.1f", $1*$2}'`
         tag=x_${x_start}_to_${x_end}
 	echo TAG $tag
