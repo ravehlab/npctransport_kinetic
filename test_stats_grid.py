01dfb98e9df673917ea4ea9bd7e16ab2cc90acc0
Barak Raveh <ravehb@cs.huji.ac.il>
2021-07-22 11:55:55 +0300

    test for custom N and C

--- test_stats_grid.py
+++ test_stats_grid.py
@@ -31,9 +31,6 @@ class TestStatsGrid(unittest.TestCase):
             number_of_processors=4)
         with open(pkl_file.name,"rb") as F:
             output = pickle.load(F)
-            for i, oi in enumerate(output):
-                print(f"i={i}")
-                print(oi)
             stats_by_passive, TSs, tags = output 
             self.assertAlmostEqual(stats_by_passive[0.01]['complexL_C'][0][0], 1.23323586e4, places=0)
             self.assertAlmostEqual(stats_by_passive[0.01]['complexL_C'][0][1], 8.97198131e3, places=0)
@@ -43,6 +40,30 @@ class TestStatsGrid(unittest.TestCase):
             self.assertAlmostEqual(TSs[0.01].v_N_L, 627e-15)
             self.assertAlmostEqual(TSs[0.01].v_C_L, 2194e-15)
 
+    def test_custom_v_N_C(self):
+        print("Testing custom volume for N and C")
+        pkl_file = tempfile.NamedTemporaryFile(suffix=".pkl")
+        print(f"Pickle file {pkl_file.name}")
+        stats_grid.get_stats_on_grid(
+            output="test",
+            passive_range=(0.01,0.15),
+            npc_traverse_range=(1,1000),
+            k_on_range=(0.001,5),
+            nx=2,
+            ny=2,
+            n_passive=2,
+            cargo_concentration_M=0.1e-6,
+            Ran_concentration_M=20e-6,
+            v_N_L=2627e-15,
+            v_C_L=194e-15,
+            equilibration_time_sec = 0.2,            
+            pickle_file = pkl_file.name,
+            number_of_processors=4)
+        with open(pkl_file.name,"rb") as F:
+            output = pickle.load(F)
+            stats_by_passive, TSs, tags = output 
+            self.assertAlmostEqual(TSs[0.01].v_N_L, 2627e-15)
+            self.assertAlmostEqual(TSs[0.01].v_C_L, 194e-15)
 
 
 if __name__ == '__main__':
